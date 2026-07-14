6f9ffcb15fbaf257a5c266df384eb14d9b051e49
rani ghotale <247193305+ranighotale8-byte@users.noreply.github.com>
2026-07-14 19:10:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest Non-Zero Number/smallest-non-zero-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int find(vector<int>& arr) {
+        // code here
+        int need = 0;
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            need = (need + arr[i] + 1)/ 2;
+        }
+    
+        return need;
+    }
+};
